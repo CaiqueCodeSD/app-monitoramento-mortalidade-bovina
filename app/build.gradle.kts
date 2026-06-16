4399cf0bcdfe95d34e7feac3a768a61280b05a72
CaiqueCodeSD <cmcaiquemonteiromedraw@gmail.com>
2026-06-15 22:11:29 -0300

    style: enhance registro screen layout with rounded components

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,10 @@ android {
 }
 
 dependencies {
+    implementation("androidx.camera:camera-core:1.4.1")
+    implementation("androidx.camera:camera-camera2:1.4.1")
+    implementation("androidx.camera:camera-lifecycle:1.4.1")
+    implementation("androidx.camera:camera-view:1.4.1")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.2.0")
     implementation("com.google.android.gms:play-services-location:21.3.0")
     implementation("com.google.android.material:material:1.13.0")
